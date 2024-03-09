1bb547460dbf7b7a48c12fa68bf1c2cec1eb5f25
pavan080600 <pavan080600@gmail.com>
2024-03-09 15:26:18 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from ./components/UserProfile/index
+
+const UserDetails = {
+  image: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'pavan',
+  role: 'developer'
+}
+
+const App = () => UserProfile
+
+export default App
